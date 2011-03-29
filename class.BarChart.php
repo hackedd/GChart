44f3ba903dcf15c35f150dd7a1a4a766d5d10bfe
Paul Hooijenga <paulhooijenga@gmail.com>
2011-03-29 11:12:10 +0000

    - Added Copyright notices - Added Id keywords

--- class.BarChart.php
+++ class.BarChart.php
@@ -1,4 +1,24 @@
 <?php
+	/*
+		This file is part of GChart - A Google Chart API Library for PHP
+		Copyright (C) 2009-2011 Paul Hooijenga
+		
+		This program is free software: you can redistribute it and/or modify
+		it under the terms of the GNU Lesser General Public License as
+		published by the Free Software Foundation, either version 3 of the
+		License, or (at your option) any later version.
+		
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU Lesser General Public License for more details.
+		
+		You should have received a copy of the GNU Lesser General Public License
+		along with this program. If not, see <http://www.gnu.org/licenses/>.
+		
+		$Id$
+	*/
+
 	class BarChart extends GChart
 	{
 		public function __construct($width, $height)
